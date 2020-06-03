ba7f69f1d5b7c48ff85196f25968d45ab6cdb057
Marco Giannini <marco.giannini@6emme.it>
2020-06-03 16:42:04 +0200

    Add header and footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import './App.css';
 import 'antd/dist/antd.css';
 import Monsters from './components/Monsters'
 
+import { Typography, Layout } from 'antd'
+
 function App() {
 
+    const { Header, Content, Footer } = Layout
+    const { Title } = Typography
+
     return (
-        <div className="App">
-            <Monsters />
-        </div>
+        <Layout className="App">
+            <Header style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}
+            ><Title style={{ color: "#fff", alignItems: "center", marginBottom: 0 }}>Who Drops that Lure?!</Title></Header>
+            <Content>
+                <Monsters />
+            </Content>
+            <Footer style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}>Who drops that lure is completely free and open source. Feel free to ping me on Discord or open an issue on Github if you're finding some wrong info.</Footer>
+        </Layout>
     );
 }
 
